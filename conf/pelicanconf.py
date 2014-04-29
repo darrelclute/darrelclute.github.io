d50bf119c68411b9951b9e8d37fa58247467cab2
Darrel Clute <darrel@darrelclute.net>
2014-04-29 14:36:04 -0600

    enabling interlinks plugin
    Signed-off-by: Darrel Clute <darrel@darrelclute.net>

--- conf/pelicanconf.py
+++ conf/pelicanconf.py
@@ -8,7 +8,7 @@ from datetime import date
 # Plugin settings, do not alter path without altering build script
 PLUGIN_PATH = 'OPENSHIFT_DATA_DIR/pelican-plugins'
 # Uncomment the following line to enable plugins, add or subtract as desired
-PLUGINS = ['multi_part', ] #'interlinks', 'summary', 'neighbors']
+PLUGINS = ['multi_part', 'interlinks', ] #'summary', 'neighbors']
 
 # General Settings
 SITEURL = u'http://www.darrelclute.net'
